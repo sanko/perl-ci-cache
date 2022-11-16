251f3f43a063cca32d2712ddda7750f4e6aeeaaf
Sanko Robinson <sanko@cpan.org>
2022-11-16 16:28:36 -0500

    Testing

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+CI::Cache - It's new $module
+
+# SYNOPSIS
+
+    use CI::Cache;
+
+# DESCRIPTION
+
+CI::Cache is ...
+
+# LICENSE
+
+Copyright (C) Sanko Robinson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Sanko Robinson <sanko@cpan.org>
---
+++ lib/CI/Cache.pm
@@ -0,0 +1,39 @@
+package CI::Cache;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+CI::Cache - It's new $module
+
+=head1 SYNOPSIS
+
+    use CI::Cache;
+
+=head1 DESCRIPTION
+
+CI::Cache is ...
+
+=head1 LICENSE
+
+Copyright (C) Sanko Robinson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Sanko Robinson E<lt>sanko@cpan.orgE<gt>
+
+=cut
+
